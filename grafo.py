6c3129faf2a13d7bc811a1bd5573d91225dcb687
gero17illescas <gero17illescas@gmail.com>
2017-11-28 19:37:43 -0300

    Cambios en grafo.py

--- grafo.py
+++ grafo.py
@@ -1,34 +1,35 @@
-class Arista(object):
-	def __init__(self,nombre,vertice_1,vertice_2,peso=None):
-		self.nombre = nombre
-		self.vertice_1 = vertice_1
-		self.vertice_2 = vertice_2        
-		self.peso = peso
+
+                memoria     agregar vertice     agregar arista  ver si a es conectado con B    sacar vertice    sacar aristas
+ma incidencia      v*e          v*E          v*E                 E                                  v*E          v*e
+ma adyacencia   v²              v²              1                1                                 v²             v²             
+li ayacenciaa   v+e             1               v                v                                v+e            V
+diccio de dic   v+e             1               1                1                                  v               1
 
 class Grafo(object):
 	def __init__(self):
-		self.vertice = {}
+		self.vertices = {}
 		self.aristas = {}
  
 	def agregar_vertice(self, vertice):
 		#Agrega un vertice al grafo
-		self.vertice[vertice] = []
+		self.vertices[vertice] = []
  
 	def borrar_vertice(self, vertice):
 		#Remueve el vertice del grafo, si no existe levanta KeyErro
 		try:
-			self.vertice.pop(vertice)
+			for keys in self.vertices.keys():
+				self.vertices[key].pop(vertice)
+			self.vertices.pop(vertice)
 		except KeyError:
 			#Si el vertice no esta en el grafo
 			pass
  
-	def agregar_arista(self, vertice_1, vertice_2,nombre,peso=None):
+	def agregar_arista(self, vertice_1, vertice_2,nombre):
 		#Agrega la arista en el vertice si este existe
 		try:
-			a = Arista(nombre,vertice_1,vertice_2,peso)
-			self.vertice[vertice_1]= nombre
-			self.vertice[vertice_2]= nombre
-			self.aristas[nombre]= a
+			self.vertices[vertice_1]= nombre
+			self.vertices[vertice_2]= nombre
+			self.aristas[nombre]= (vertice_1,vertice_2)
 		except KeyError:
 			#Si el vertice no esta en el grafo
 			pass
@@ -36,71 +37,50 @@ class Grafo(object):
 	def borrar_arista(self, arista):
 		#Remueve la arista si esta en el grafo
 		try:
-			a = self.aristas[arista]
-			self.vertice[a.vertice_1].pop(a.nombre)
-			self.vertice[a.vertice_2].pop(a.nombre)
-			self.aristas.pop(arista)
+			a=self.aristas.pop(arista)
+			if(a):
+    		(vertice_1,vertice_2)=a
+				self.vertices[vertice_1].pop(arista)
+				self.vertices[vertice_2].pop(arista)
+			
 		except KeyError:
 			#Si el vertice no esta en el grafo
 			pass
  
-	def get_arista(self, arista):
+	def get_aristas(self, arista):
 		#Devuelve la arista si esta en el grafo, caso contrario None
 		try:
-			return self.aristas[arista]
+			return self.aristas
 		except KeyError:
 			pass
-	def get_vertice(self, vertice):
+	def get_vertices(self, vertice):
 		#Devuelve el vertice si esta en el grafo, caso contrario None
 		try:
-			return self.vertice[vertice]
+			return self.vertices
 		except KeyError:
 			pass
 
 	def get_adyacentes(self,vertice):
-		#Devulve una dict de nombres de aristas, si el vertice esta en 
-		#el grafo. None en caso contrario
+    		
 		try:
-			return self.vertice[vertice]
+			lista = []
+			for arista in self.vertices[vertice]:
+				(vertice_1,vertice_2)=self.aristas[arista]
+				if vertice_1 is vertice:
+    				lista.append(vertice_2)
+				else:
+    				lista.append(vertice_1)
+			return lista
 		except KeyError:
 			#Si el vertice no esta en el grafo
 			pass
 
 	def son_adyacentes(self,vertice_1,vertice_2):
 		try:
-			for a in self.vertice[vertice_1]
-				if a.vertice_2==vertice_2
+			for adyacente in get_adyacentes(vertice_1):
+    			if(adyacente is vertice_2)
 					return True
+			return False
 		except KeyError:
 			#Si el vertice no esta en el grafo
 			return False
-
-
-def dijsktra(graph, initial):
-  visited = {initial: 0}
-  path = {}
-
-  nodes = set(graph.nodes)
-
-  while nodes: 
-	min_node = None
-	for node in nodes:
-	  if node in visited:
-		if min_node is None:
-		  min_node = node
-		elif visited[node] < visited[min_node]:
-		  min_node = node
-
-	if min_node is None:
-	  break
-
-	nodes.remove(min_node)
-	current_weight = visited[min_node]
-
-	for edge in graph.edges[min_node]:
-	  weight = current_weight + graph.distance[(min_node, edge)]
-	  if edge not in visited or weight < visited[edge]:
-		visited[edge] = weight
-		path[edge] = min_node
-
-return visited, path
