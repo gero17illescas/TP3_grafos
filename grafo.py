a8138de447aad9dfc94b7fa057b96822f814271e
gero17illescas <gero17illescas@gmail.com>
2017-11-30 22:50:24 -0300

    Terminaado kevin_bacon

--- grafo.py
+++ grafo.py
@@ -1,81 +1,89 @@
 class Grafo(object):
-	def __init__(self):
-		self.vertices = {}
-		self.aristas = {}
+    """
+    Representacion de un grafo
+    """
+    def __init__(self):
+        self.vertices = {}
+        self.aristas = {}
  
-	def agregar_vertice(self, vertice):
-		#Agrega un vertice al grafo
-		self.vertices[vertice] = []
+    def agregar_vertice(self, vertice):
+        """
+        Agrega un vertice al grafo
+        """
+        self.vertices[vertice] = []
  
-	def borrar_vertice(self, vertice):
-		#Remueve el vertice del grafo, si no existe levanta KeyErro
-		try:
-			for keys in self.vertices.keys():
-				self.vertices[key].pop(vertice)
-			self.vertices.pop(vertice)
-		except KeyError:
-			#Si el vertice no esta en el grafo
-			pass
+    def borrar_vertice(self, vertice):
+        """
+        Remueve el vertice del grafo, si no existe levanta KeyError
+        """
+        try:
+            for key in self.vertices.keys():
+                self.vertices[key].pop(vertice)
+            self.vertices.pop(vertice)
+        except KeyError:
+            #Si el vertice no esta en el grafo
+            pass
  
-	def agregar_arista(self, vertice_1, vertice_2,nombre):
-		#Agrega la arista en el vertice si este existe
-		try:
-			self.vertices[vertice_1].append(nombre)
-			self.vertices[vertice_2].append(nombre)
-			self.aristas[nombre]= (vertice_1,vertice_2)
-		except KeyError:
-			#Si el vertice no esta en el grafo
-			pass
+    def agregar_arista(self, vertice_1, vertice_2,nombre):
+        """
+        Agrega la arista en el vertice si este existe
+        """
+        try:
+            self.vertices[vertice_1].append(nombre)
+            self.vertices[vertice_2].append(nombre)
+            self.aristas[nombre]= (vertice_1,vertice_2)
+        except KeyError:
+            #Si el vertice no esta en el grafo
+            pass
  
-	def borrar_arista(self, arista):
-		#Remueve la arista si esta en el grafo
-		try:
-			a=self.aristas.pop(arista)
-			if(a):
-    		(vertice_1,vertice_2)=a
-				self.vertices[vertice_1].pop(arista)
-				self.vertices[vertice_2].pop(arista)
-			
-		except KeyError:
-			#Si el vertice no esta en el grafo
-			pass
+    def borrar_arista(self, arista):
+        #Remueve la arista si esta en el grafo
+        try:
+            a=self.aristas.pop(arista)
+            if a:
+                (vertice_1,vertice_2)=a
+                self.vertices[vertice_1].pop(arista)
+                self.vertices[vertice_2].pop(arista)
+            
+        except KeyError:
+            #Si el vertice no esta en el grafo
+            pass
  
-	def get_aristas(self, arista):
-		#Devuelve la arista si esta en el grafo, caso contrario None
-		try:
-			return self.aristas
-		except KeyError:
-			pass
-	def get_vertices(self, vertice):
-		#Devuelve el vertice si esta en el grafo, caso contrario None
-		try:
-			return self.vertices
-		except KeyError:
-			pass
+    def get_aristas(self):
+        #Devuelve la arista si esta en el grafo, caso contrario None
+        try:
+            return self.aristas
+        except KeyError:
+            pass
+    def get_vertices(self):
+        #Devuelve el vertice si esta en el grafo, caso contrario None
+        try:
+            return self.vertices
+        except KeyError:
+            pass
 
-	def get_adyacentes(self,vertice):
-    		
-		try:
-			lista = []
-			for arista in self.vertices[vertice]:
-				(vertice_1,vertice_2)=self.aristas[arista]
-				if vertice_1 is vertice:
-    				lista.append(vertice_2)
-				else:
-    				lista.append(vertice_1)
-			return lista
-		except KeyError:
-			#Si el vertice no esta en el grafo
-			pass
+    def get_adyacentes(self,vertice):
+            
+        try:
+            lista = []
+            for arista in self.vertices[vertice]:
+                (vertice_1,vertice_2)=self.aristas[arista]
+                if vertice_1 is vertice:
+                    lista.append(vertice_2)
+                else:
+                    lista.append(vertice_1)
+            return lista
+        except KeyError:
+            #Si el vertice no esta en el grafo
+            pass
 
-	def son_adyacentes(self,vertice_1,vertice_2):
-		try:
-			for adyacente in get_adyacentes(vertice_1):
-    			if(adyacente is vertice_2)
-					return True
-			return False
-		except KeyError:
-			#Si el vertice no esta en el grafo
-			return False
-	def arista_en_comun(self,vertice,vertice_2):
-		if son_adyacentes(vertice_1,ver)
+    def son_adyacentes(self,vertice_1,vertice_2):
+        try:
+            for adyacente in get_adyacentes(vertice_1):
+                if adyacente is vertice_2:
+                    return True
+            return False
+        except KeyError:
+            #Si el vertice no esta en el grafo
+            return False
+
--- kevin_bacon.py
+++ kevin_bacon.py
@@ -1,61 +1,118 @@
-import seis_grados
+# coding=utf-8
+from seis_grados import *
+import sys
+def camino_hasta_KB(dataset, actor):
+    """
+    Imprime el camino más corto con el cual se llega desde cualquier 
+    actor hasta Kevin Bacon.
+    """
+    path = camino(dataset, 'Kevin Bacon', actor)
+    for (actor_1, actor_2, pelicula) in path:
+        print(actor_1+" actuó con "+actor_2+" en "+pelicula[0]+".")
 
-def camino_hasta_KB(dataset,actor):
-	camino = interfaz(camino,'Kevin Bacon',actor)
-	for (actor_1,actor_2,pelicula) in camino:
-		print(actor_1+" actuó con "+actor_2+" en "+pelicula[0]+".")
+def bacon_number(dataset, actor):
+    """
+    Imprime el Kevin Bacon Number del actor recibido. 
+    """
+    path = camino(dataset, 'Kevin Bacon', actor)
+    if not pertenece_actor(dataset, actor):
+        print("El actor ingresado o es incorrecto o no se encuentra en la base de datos.")
+        return
+    kbn=-1
+    if path:
+        kbn=len(path)
+    print(actor+" tiene un Kevin Bacon Number igual a "+kbn+".")
+    
+def bacon_number_mayor_a_6(dataset,  actor):
+    """
+    Imprime la cantidad de actores (¿existirán?) a una distancia mayor
+    a 6 pasos de Kevin Bacon. De no existir actores a más pasos que 6,
+    se imprime un mensaje acorde. En este numero no influyen la cantidad
+    de actores con un KBN infinito.
+    """
+    actores = actores_a_mayor_distancia(dataset, actor, 6)
+    if not actores:
+        print("No existen actores cuyo KBN a % sea mayor a 6", actor)
+        return
+    else:
+        aux = {}
+        for actor, cercania in actores:
+            if cercania in aux:
+                    aux[cercania].append(actor)
+            else:
+                    aux[cercania]=0
+        for cercania in aux:
+            print("Con KBN igual a %: %", cercania, aux[cercania])
 
-def bacon_number(dataset,actor):
-	camino = camino(dataset,'Kevin Bacon',actor)
-	if not pertence_actor(actor):
-		print("El actor ingresado o es incorrecto o no se encuentra en la base de datos.")
-		return
-	kbn=-1
-	if camino:
-		kbn=len(camino)
-	print(actor+" tiene un Kevin Bacon Number igual a "+kbn+".")
-	
-def bacon_number_mayor_a_6(dataset,actor):
-	actores = 
-	Bacon Number mayor a 6
+def bacon_number_infinito(dataset):
+    """
+    Imprime la cantidad de actores con un KBN infinito. De no haber,  se
+    debe imprimir un mensaje acorde.
+    """
+    raise ValueError
 
-	Imprime la cantidad de actores (¿existirán?) a una distancia mayor a 6 pasos de Kevin Bacon. De no existir actores a más pasos que 6, se imprime un mensaje acorde. En este numero no influyen la cantidad de actores con un KBN infinito.
+def KBN_promedio(dataset):
+    """
+    Imprime el Kevin Bacon Number promedio de todos los actores en
+    la base de datos.
 
-	bacon_number_mayor_a_6
-	>>> Los actores con un KBN mayor a 6 son:
-	>>> Con KBN igual a 6: N actores
-	>>> Con KBN igual a 7: N actores
-	>>> ...
+    PRE: Recibe el dataset.
+    """
+    promedio = estadisticas(dataset)[2]
+    print("El Kevin Bacon Number promedio es %", promedio)
 
-	Bacon Number infinito
-
-	Imprime la cantidad de actores (¿existirán?) con un KBN infinito. De no haber, se debe imprimir un mensaje acorde.
-
-	bacon_number_infinito
-	>>> Los actores con un Bacon Number infinito son N
-
-	Bacon Number promedio
-
-	Imprime el Kevin Bacon Number promedio. En este numero no influyen la cantidad de actores con un KBN infinito, pero si lo hace el KBN de Kevin Bacon.
-
-	KBN_promedio
-	>>> El Kevin Bacon Number promedio es N
-
+def similares_a_KB(dataset, n):
+    print("Los % actores más similares KB son %", n, similares(dataset, 'Kevin Bacon', n))
+    
+def popularidad_contra_KB(dataset, actor):
+    kb=popularidad(dataset,  'Kevin Bacon')
+    ac=popularidad(dataset,  actor)
    
-def similares_a_KB(dataset,n):
-	similares = interfaz(similares,n)
-	print("Los % actores más similares KB son %",n,similares)
-	
-def popularidad_contra_KB(dataset,actor):
-	kb=interfaz(popularidad, 'Kevin Bacon')
-	ac=interfaz(popularidad, actor)
-   
-	print("% es un % de lo popular que es Kevin Bacon" % (actor,(ac*100/kb)))
-	
+    print("% es un % de lo popular que es Kevin Bacon" % (actor, (ac*100/kb)))
+    
 def cantidad_actores(dataset):
-	print("El dataset contiene % actores" % interfaz(estadisticas)[1])
+    print("El dataset contiene % actores" % estadisticas(dataset)[1])
 
 def cantidad_peliculas(dataset):
-	print("El dataset contiene % películas" % interfaz(estadisticas)[0])
+    print("El dataset contiene % películas" % estadisticas(dataset)[0])
 
-def main()
+    
+def main():
+    """Funcion principal"""
+   
+  
+    file = sys.argv[1]
+    dataset = grafo_crear(file)
+    print("Cargado.\n")
+    for acotres in dataset.get_vertices():
+        print(acotres)
+    for pelicula in dataset.get_aristas():
+        print(pelicula)
+    entrada= input().split(" ")
+    funciones = ['camino_hasta_KB', 'bacon_number', 'bacon_number_mayor_a_6', 
+    'similares_a_KB', 'popularidad_contra_KB', 'bacon_number_infinito', 
+    'KBN_promedio', 'cantidad_actores', 'cantidad_peliculas']
+    while entrada == "" or entrada[0] not in funciones:
+        entrada = input("No valido.Vuelva a ingresar.\n")
+    
+    if entrada[0] == 'camino_hasta_KB':
+        camino_hasta_KB(dataset,'Reeves Keanu')
+    if entrada[0] == 'bacon_number':
+        bacon_number(dataset, entrada[1])
+    if entrada[0] == 'bacon_number_mayor_a_6':
+        bacon_number_mayor_a_6(dataset, entrada[1])
+    if entrada[0] == 'similares_a_KB':
+        if entrada[1].isdigit():
+            similares_a_KB(dataset, entrada[1])
+    if entrada[0] == 'popularidad_contra_KB':
+        popularidad_contra_KB(dataset, entrada[1])
+    if entrada[0] == 'bacon_number_infinito':
+        bacon_number_infinito(dataset)
+    if entrada[0] == 'KBN_promedio':
+        KBN_promedio(dataset)
+    if entrada[0] == 'cantidad_actores':
+        cantidad_actores(dataset)
+    if entrada[0] == 'cantidad_peliculas':
+        cantidad_peliculas(dataset)
+
+main()
